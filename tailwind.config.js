e8ccccd6d6822932dbec4b07488bb58450430f04
diptowebhero <diptodas1672@gmail.com>
2023-02-05 00:48:33 +0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.html"],
+  theme: {
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "976px",
+      xl: "1240px",
+    },
+    extend: {
+      fontFamily: {
+        Manrope: ["Manrope", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
